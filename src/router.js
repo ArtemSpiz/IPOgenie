e68a2dde203462a04ccc6d8fa3560c61e5f0e368
ArtemSpiz <artemijspizhav@gmail.com>
2025-12-03 18:36:14 +0200

    home

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "@/pages/Home.vue";
 import News from "./pages/News.vue";
 import Tokenomics from "./pages/Tokenomics.vue";
@@ -14,7 +14,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
