7084223e41e545f372f54e51ec479fff6445e784
ArtemSpiz <artemijspizhav@gmail.com>
2025-12-05 20:06:02 +0200

    BuyNowPage

--- src/components/Home/Hero.vue
+++ src/components/Home/Hero.vue
@@ -2,15 +2,30 @@
 import CustomButton from "@/ui/CustomButton.vue";
 import logoPartner1 from "@/assets/img/Home/logoPartner1.png";
 import logoPartner2 from "@/assets/img/Home/logoPartner2.png";
+import VideoBgHero from "@/assets/img/Home/VideoBgHero.mp4";
 </script>
 
 <template>
   <div class="px-[7px] pt-[3px] h-screen max-md:px-[10px] max-md:pt-[5px]">
     <div
-      class="flex flex-col gap-10 items-center justify-end rounded-[60px] max-md:rounded-[30px] overflow-hidden bg-cover bg-no-repeat bg-center bg-[url('@/assets/img/Home/HeroBg.png')] w-full h-full pb-[200px] max-2xl:pb-[100px] max-xl:pb-[80px] max-lg:pb-[60px] max-md:pb-[40px] max-md:bg-[url('@/assets/img/Home/HeroBgMob.png')] max-md:gap-0 max-md:px-12 max-xl::rounded-[30px]"
+      class="flex relative flex-col gap-10 items-center justify-end rounded-[60px] max-md:rounded-[30px] overflow-hidden w-full h-full pb-[200px] max-2xl:pb-[100px] max-xl:pb-[80px] max-lg:pb-[60px] max-md:pb-[40px] max-md:gap-0 max-md:px-12 max-xl::rounded-[30px]"
     >
+      <img
+        :src="HeroBg"
+        class="absolute inset-0 w-full h-full object-cover max-md:hidden"
+        alt="Background fallback"
+      />
+      <video
+        class="absolute inset-0 w-full h-full object-cover"
+        autoplay
+        muted
+        loop
+        playsinline
+      >
+        <source :src="VideoBgHero" type="video/mp4" />
+      </video>
       <div
-        class="flex flex-col items-center max-md:justify-center max-md:h-[calc(100%-150px)]"
+        class="flex flex-col z-20 items-center max-md:justify-center max-md:h-[calc(100%-150px)]"
       >
         <div
           class="text-[150px] text-center font-bold tracking-[-6px] max-2xl:text-[120px] max-xl:text-[100px] max-lg:text-7xl max-lg:tracking-[-1.6px] max-md:text-[42px]"
